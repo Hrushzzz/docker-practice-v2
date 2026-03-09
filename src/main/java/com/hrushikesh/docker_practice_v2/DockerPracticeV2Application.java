5224a4e5500b2875a7571993bf05a722bb3172b8
SpiderMan <hrushikesh.tk@gmail.com>
2026-03-09 23:21:38 +0530

    Docker demo initial setup

---
+++ src/main/java/com/hrushikesh/docker_practice_v2/DockerPracticeV2Application.java
@@ -0,0 +1,13 @@
+package com.hrushikesh.docker_practice_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerPracticeV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerPracticeV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hrushikesh/docker_practice_v2/DockerPracticeV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrushikesh.docker_practice_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerPracticeV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
